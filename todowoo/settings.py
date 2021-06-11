10115b04e4efb606b104316783fa0b7848b41a91
yash-2115 <mailforyash21@gmail.com>
2021-06-11 22:08:09 +0530

    asa

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -134,9 +134,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = [
+    'todowoo/static'
+]
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
