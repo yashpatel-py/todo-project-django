b927dfcdf616c5964a417eb6a97d276ac6f56253
Yash Patel <mailforyash04@gmail.com>
2022-05-25 22:05:28 +0530

    Added database conf

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -86,13 +86,23 @@ WSGI_APPLICATION = 'todowoo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dpnj86mvipt57',                      
+        'USER': 'oroghfgtomaxni',
+        'PASSWORD': '3346354c03d563c9e907aff1772bfb9ad0cd45387063bfc5935baeff7f184950',
+        'HOST': 'ec2-34-193-113-223.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
